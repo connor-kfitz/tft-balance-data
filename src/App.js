39b9dd9ef9cfe11155ca5731b07095d3243ca5d7
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-09-10 14:25:46 -0400

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Nav from "./components/navbar";
+import Homepage from "./pages/homepage";
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Nav/>
+          <Routes>
+            <Route path="/" element={<Homepage/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "../styles/homepage.css"
+
 
 export default function Homepage() {
 
     return (
-        <div>
-        </div>
+        <section>
+            <div id="homepageCont">
+                <p>Welcome to the TFT data collection app!</p>
+                <p>Select one of the tabs above to sort items and traits, based on current balance state.  Submit your results, then navigate to the statistics page to view the overall results.</p>
+            </div>
+        </section>
     );
 }
