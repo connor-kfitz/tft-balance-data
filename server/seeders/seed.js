0493d6f702d8e6eaa6af152d73266010e0c04333
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-09-16 18:09:32 -0400

    Added Item Seed Data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,21 @@
 const db = require('../config/connection');
-const { Profile } = require('../models');
-const profileSeeds = require('./profileSeeds.json');
+const { Items, Synergies } = require('../models');
+
+const itemsSeeds = require('./itemsSeeds.json');
+const synergiesSeeds = require('./synergiesSeeds.json');
 
 db.once('open', async () => {
+  await Items.deleteMany({});
+  await Synergies.deleteMany({});
+
   try {
-    await Profile.deleteMany({});
-    await Profile.create(profileSeeds);
+    await Items.create(itemsSeeds);
+    await Synergies.create(synergiesSeeds);
 
     console.log('all done!');
     process.exit(0);
   } catch (err) {
     throw err;
   }
+
 });
