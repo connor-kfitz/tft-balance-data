9c8e81821e419026d81f5ad64b491d8bf79e5843
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-09-17 13:24:28 -0400

    Query Items Working in React

--- client/src/pages/Statistics.jsx
+++ client/src/pages/Statistics.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "../styles/statistics.css"
 
+import { useQuery } from "@apollo/client";
+
+import { QUERY_ITEMS } from "../utils/queries";
+
 export default function Statistics() {
 
+    const { loading, data } = useQuery(QUERY_ITEMS);
+
+    const items = data?.items || [];
+
+    console.log('First Bit')
+    console.log(data)
+    console.log(items);
+
     return (
-        <section>
-            <h1>Stats</h1>
-        </section>
+        <div>
+            {loading ? (
+                <div>Loading...</div>
+            ): (
+                <div>Data {items[0].id}</div>
+            )}
+        </div>
     );
 }
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,18 +5,9 @@ const resolvers = {
         items: async () => {
           return Item.find();
         },
-
-        // item: async(parent, { itemId }) => {
-        //   return Item.findOne({ _id: itemId });
-        // },
-
         synergies: async () => {
           return Synergy.find();
-        },
-
-        // synergy: async(parent, { synergyId }) => {
-        //   return Synergy.findOne({ _id: synergyId });
-        // }
+        }
       },
 };
 
